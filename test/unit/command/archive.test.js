fab2399fe17eb4e1e7faafa3ec967c9d9cc8d587
Igor Korovchenko <igsekor@gmail.com>
2021-04-11 19:04:51 +0000

    Add test for builder function

--- test/unit/command/archive.test.js
+++ test/unit/command/archive.test.js
@@ -1,7 +1,10 @@
 'use strict'
 
-const { expect } = require('chai')
-const { handler: archive, messages } = require('../../../core/command/archive')
+const chai = require('chai')
+
+const expect = chai.expect
+
+const { handler: archive, builder, messages } = require('../../../core/command/archive')
 
 describe('Archive command', () => {
   describe('types', () => {
@@ -16,6 +19,42 @@ describe('Archive command', () => {
 
       expect(typeof start).to.be.eq('string')
       expect(typeof end).to.be.eq('string')
+    })
+  })
+
+  describe('builder', () => {
+    it('builder must be a function', () => {
+      expect(typeof builder).to.be.eq('function')
+    })
+
+    it('builder must process yargs object', () => {
+      const obj = {
+        options: (yargsOptions) => {
+          return yargsOptions
+        }
+      }
+      const yargsOptions = {
+        output: {
+          alias: 'o',
+          type: 'string',
+          default: 'presentation.zip',
+          describe: 'Archive name'
+        },
+        files: {
+          alias: 'f',
+          array: true,
+          type: 'string',
+          describe: 'List of files that will get the build'
+        }
+      }
+      expect(builder(obj).output.alias).to.be.eq(yargsOptions.output.alias)
+      expect(builder(obj).output.type).to.be.eq(yargsOptions.output.type)
+      expect(builder(obj).output.default).to.be.eq(yargsOptions.output.default)
+      expect(builder(obj).output.describe).to.be.eq(yargsOptions.output.describe)
+      expect(builder(obj).files.alias).to.be.eq(yargsOptions.files.alias)
+      expect(builder(obj).files.array).to.be.eq(yargsOptions.files.array)
+      expect(builder(obj).files.type).to.be.eq(yargsOptions.files.type)
+      expect(builder(obj).files.describe).to.be.eq(yargsOptions.files.describe)
     })
   })
 })
