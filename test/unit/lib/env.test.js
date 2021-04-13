38467569936b4195b612ab8df95b971ada1fa351
Igor Korovchenko <igsekor@gmail.com>
2021-04-13 20:40:48 +0000

    Add new tests for env lib

--- test/unit/lib/env.test.js
+++ test/unit/lib/env.test.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const util = require('util')
+const path = require('path')
+
 const chai = require('chai')
 
 const expect = chai.expect
@@ -10,6 +13,27 @@ describe('Env lib', () => {
   describe('types', () => {
     it('getEnv must be a function', () => {
       expect(typeof getEnv).to.be.eq('function')
+    })
+  })
+
+  describe('function', () => {
+    const mock = require('mock-fs')
+
+    beforeEach(() => {
+      mock({
+        'mocked-shower': mock.load(path.resolve(__dirname, '../../../templates/presentation'))
+      })
+    })
+
+    afterEach(() => {
+      mock.restore()
+    })
+
+    it('getEnv should return right path', () => {
+      const dir = `${process.cwd()}/mocked-shower`
+      const expectedResult = { path: '/workspaces/cli/mocked-shower' }
+      const result = getEnv(dir)
+      expect(util.isDeepStrictEqual(result, expectedResult)).to.be.eq(true)
     })
   })
 })
