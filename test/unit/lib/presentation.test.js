0aa3bd6fe0f9a38c6b2a58711cb1b16a020a483c
Igor Korovchenko <igsekor@gmail.com>
2021-04-13 20:41:28 +0000

    Add mock tests for presentation

--- test/unit/lib/presentation.test.js
+++ test/unit/lib/presentation.test.js
@@ -1,8 +1,12 @@
 'use strict'
 
+const chaiStream = require('chai-stream')
+const path = require('path')
+
 const chai = require('chai')
 
 const expect = chai.expect
+chai.use(chaiStream)
 
 const { loadPresentationFiles } = require('../../../core/lib/presentation')
 
@@ -10,6 +14,30 @@ describe('Presentation lib', () => {
   describe('types', () => {
     it('loadPresentationFiles must be a function', () => {
       expect(typeof loadPresentationFiles).to.be.eq('function')
+    })
+  })
+
+  describe('function', () => {
+    const mock = require('mock-fs')
+
+    beforeEach(() => {
+      mock({
+        'mocked-shower': mock.load(path.resolve(__dirname, '../../../templates/presentation')),
+        'mocked-shower/node_modules/@shower/core/dist/shower.js': '(() => {})'
+      })
+    })
+
+    afterEach(() => {
+      mock.restore()
+    })
+
+    it('loadPresentationFiles should return a NodeJS.ReadableStream', () => {
+      const dir = `${process.cwd()}/mocked-shower`
+      process.chdir(dir)
+
+      const stream = loadPresentationFiles()
+      // eslint-disable-next-line no-unused-expressions
+      expect(stream).to.be.a.ReadableStream
     })
   })
 })
